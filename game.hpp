6f0acf590154f1be47cf9d48c18e7cd492baad4c
GingerSchnitzel <141836349+GingerSchnitzel@users.noreply.github.com>
2024-04-17 18:51:13 +0300

    Updated game functionality

--- game.hpp
+++ game.hpp
@@ -5,7 +5,7 @@
 #include <random>
 #include <iostream>
 
-std::pair<size_t,size_t> selectRandomPosition()
+std::pair<size_t, size_t> selectRandomPosition()
 {
 	static std::random_device rd{};                                                 // Random device for seeding the twister
 	static std::seed_seq ss{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() };      // std::seed_seq resolves underseeding issues 
@@ -14,7 +14,7 @@ std::pair<size_t,size_t> selectRandomPosition()
 	size_t randomRow = dist(engine);                                                // generates a random integer for row
 	size_t randomColumn = dist(engine);                                             // generates a random integer for column
 	std::pair<size_t, size_t> rowColumn = { randomRow, randomColumn };
-	
+
 	// all made static in order not to create objects all over again when function is called
 	return rowColumn;
 }
@@ -27,56 +27,31 @@ std::pair<size_t,size_t> selectRandomPosition()
 // Then, if the users wants to keep playing, the winner gets to choose first in a new game (either the user or the machine)
 // The machine will look at the available spots withing the board and mark one at its turn with the assigned symbol
 // The first one to fill 3 spots in a row with his symbol, wins.
+static bool firstGame{ true };
 
 class TicTacToeSession
 {
 private:
 
-	std::array<std::array<char, 3>, 3> board{};
+	std::array<std::array<char, 3>, 3> board
+	{
+	
+	};
 	std::array <char, 2> symbols{ 'X', 'O' };
-	static bool firstGame;
+	char userSymbol{};
+	char machineSymbol{};
+
 
 public:
 
 	char getUserSymbol()
 	{
-
+		std::cout << "Enter your preffered symbol (X or O):";
 		while (true)
 		{
-			std::cout << "Enter your preffered symbol (X or O):";
+
 			char c{};
 			std::cin >> c;
-
-			if (std::cin.fail())
-			{
-				std::cin.clear();
-				std::cout << "That wasn't a valid input. Please try again.\n";
-				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				continue;
-			}
-
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-			if (c != symbols[0] || c != symbols [1])
-			{
-				std::cout << "That wasn't a valid input. Please try again.\n";
-				continue;
-			}
-
-			return c;
-		}
-
-	}
-
-	std::pair<size_t, size_t> getSymbolPosition()
-	{
-
-		while (true)
-		{
-			std::cout << "Enter the position where you want to place your chosen symbol: \n";
-			std::cout << "Row: ";
-			size_t row{};
-			std::cin >> row;
 
 			if (std::cin.fail())
 			{
@@ -93,24 +68,271 @@ public:
 				std::cout << "That wasn't a valid input. Please try again.\n";
 				continue;
 			}
-
-			return ;
+			userSymbol = c;
+			return c;
 		}
+
 	}
 	// if the user has chosen a symbol, the machine should be assigned the other
-
-	char determineMachineSymbol(char& userGuess)
+	char determineMachineSymbol(const char& userGuess)
 	{
-		char machineSymbol{};
 		switch (userGuess)
-		{ 
-			case 'X':
-				machineSymbol = 'O';
-			case 'O':
-				machineSymbol = 'X';
+		{
+		case 'X':
+			machineSymbol = 'O';
+		case 'O':
+			machineSymbol = 'X';
 		}
 	}
+
 	// when the player and the machine have a symbol, the game should start
+	// get the user prompt for the position of the symbol (has to be called twice for Row/Column)
+	size_t getPositionIndex(const std::string& positionName)
+	{
+		std::cout << "Enter the position for your symbol: \n";
+		while (true)
+		{
+			std::cout << positionName << " (1, 2, 3): ";
+			size_t index{};
+			std::cin >> index;
+
+			if (std::cin.fail())
+			{
+				std::cin.clear();
+				std::cout << "That wasn't a valid input. Please try again.\n";
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				continue;
+			}
+
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
 
+			if (index != 1 || index != 2 || index != 3)
+			{
+				std::cout << "That wasn't a valid input. Please try again.\n";
+				continue;
+			}
+
+			return index;
+		}
+	}
+
+
+	//the prompt, both in the case of the user or the machine, should be valitated and only if the spot is empty it should be filled. else, the user and 
+	// and the machine, respectively, should generate a new prompt
+
+	void positionAvailable(const std::pair<size_t, size_t>& rowColumn, char symbol)
+	{
+		while (true)
+		{
+
+				if (symbol == userSymbol)
+				{
+					if (board[rowColumn.first][rowColumn.second] != 0)
+					{
+						std::cout << "That spot is already taken! Please choose another! \n";
+						size_t row{ getPositionIndex("Row") };
+						size_t column{ getPositionIndex("Column") };
+						continue;
+					}
+					else
+					{
+						board[rowColumn.first][rowColumn.second] = userSymbol;
+						return;
+					}
+				}
+				else if (symbol == machineSymbol)
+				{ 
+					if (board[rowColumn.first][rowColumn.second] != 0)
+					{
+						selectRandomPosition();
+						continue;
+					}
+					else
+					{
+						board[rowColumn.first][rowColumn.second] = machineSymbol;
+						return;
+					}
+
+				}
+			
+		}
+
+	}
+
+	bool winner()
+	{
+		if (board[0][0] == board[0][1] == board[0][2])
+		{
+			if (board[0][0] == userSymbol)
+			{
+				std::cout << "Congrats, you have won!\n";
+				return true;
+			}
+			else if (board[0][0] == machineSymbol)
+			{
+				std::cout << "You have lost! \n";
+				return false;
+			}
+		}
+
+		if (board[1][0] == board[1][1] == board[1][2])
+		{
+			if (board[1][0] == userSymbol)
+			{
+				std::cout << "Congrats, you have won!\n";
+				return true;
+			}
+			else if (board[1][0] == machineSymbol)
+			{
+				std::cout << "You have lost! \n";
+				return false;
+
+			}
+		}
+
+		if (board[2][0] == board[2][1] == board[2][2])
+		{
+			if (board[2][0] == userSymbol)
+			{
+				std::cout << "Congrats, you have won!\n";
+				return true;
+			}
+			else if (board[2][0] == machineSymbol)
+			{
+				std::cout << "You have lost! \n";
+				return false;
+
+			}
+		}
+
+		if (board[0][0] == board[1][0] == board[2][0])
+		{
+			if (board[0][0] == userSymbol)
+			{
+				std::cout << "Congrats, you have won!\n";
+				return true;
+			}
+			else if (board[0][0] == machineSymbol)
+			{
+				std::cout << "You have lost! \n";
+				return false;
+
+			}
+		}
+
+		if (board[0][1] == board[1][1] == board[2][1])
+		{
+			if (board[0][1] == userSymbol)
+			{
+				std::cout << "Congrats, you have won!\n";
+				return true;
+			}
+			else if (board[0][1] == machineSymbol)
+			{
+				std::cout << "You have lost! \n";
+				return false;
+
+			}
+		}
+
+		if (board[0][2] == board[1][2] == board[2][2])
+		{
+			if (board[0][2] == userSymbol)
+			{
+				std::cout << "Congrats, you have won!\n";
+				return true;
+			}
+			else if (board[0][2] == machineSymbol)
+			{
+				std::cout << "You have lost! \n";
+				return false;
+
+			}
+		}
+
+		if (board[0][0] == board[1][1] == board[2][2])
+		{
+			if (board[0][0] == userSymbol)
+			{
+				std::cout << "Congrats, you have won!\n";
+				return true;
+			}
+			else if (board[0][0] == machineSymbol)
+			{
+				std::cout << "You have lost! \n";
+				return false;
+
+			}
+		}
+
+		if (board[0][2] == board[1][1] == board[2][0])
+		{
+			if (board[0][2] == userSymbol)
+			{
+				std::cout << "Congrats, you have won!\n";
+				return true;
+			}
+			else if (board[0][2] == machineSymbol)
+			{
+				std::cout << "You have lost! \n";
+				return false;
+
+			}
+		}
+	}
+
+	void printBoard()
+	{
+		for (size_t i{ 0 }; i < board.size(); ++i)
+		{
+			for (size_t j{ 0 }; j < board.size(); j++)
+			{
+
+				if (j == 2)
+				{
+					std::cout << board[i][j];
+					continue;
+				}
+				else
+				{
+					std::cout << board[i][j] << '  ' << '|' << '  ';
+					continue;
+				}
+
+			}
+			std::cout << "\n";
+			if (i == 0 || i == 1)
+			{
+				std::cout << "------";
+			}
+			std::cout << "\n";
+		}
+
+	}
+
+
+	// if user wins and wants to play again, he will start the game first (else the machine will start)
+
+
+	// if he wants to play again, chooser also gets to choose symbol again
+
+
+
+
+
+
+	//Play game
+	void playGame()
+	{
+		std::cout << "Welcome to Tic Tac Toe! \n";
+		std::cout << "You are going to take turns with me (your PC) in placing the symbol you're going to pick (X or O) at a position of your choosing. \n";
+		std::cout << "The first one of us that gets 3 of his/her marks in a row (up, down, across, or diagonally) is the winner. \n";
+		userSymbol = getUserSymbol();
+		machineSymbol = determineMachineSymbol(userSymbol);
+
+		
+
+
+	}
 };
